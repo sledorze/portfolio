1e49fc55249ab569f07424dad2babc084de5c2a0
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2024-03-15 15:44:01 +0100

    try to fix config

--- next.config.js
+++ next.config.js
@@ -66,6 +66,7 @@ module.exports = () => {
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
+    basePath: '/portfolio',
     images: {
       remotePatterns: [
         {
